87a56051d9944e2b152bf1dd4f302a3c39dbbb6e
Dhian <muthidp@gmail.com>
2024-04-07 02:04:13 +1100

    Added simple test

--- spec/math.spec.js
+++ spec/math.spec.js
@@ -4,6 +4,14 @@ const httpStatus = require('http-status')
 
 describe('Math', () => {
   describe('Addition', () => {
+    it('should return 400 if num1 or num2 is not given', (done) => {
+      request(app)
+        .post('/math/add')
+        .send({ num1: 1 })
+        .expect(httpStatus.BAD_REQUEST)
+        .end((error) => (error) ? done.fail(error) : done())
+    })
+
     it('should return 400 if num1 or num2 is not number', (done) => {
       request(app)
         .post('/math/add')
@@ -16,6 +24,33 @@ describe('Math', () => {
       request(app)
         .post('/math/add')
         .send({ num1: 1, num2: 2 })
+        .expect({ result: 3 })
+        .expect(httpStatus.OK)
+        .end((error) => (error) ? done.fail(error) : done())
+    })
+  })
+
+  describe('Division', () => {
+    it('should return 400 if num1 or num2 is not number', (done) => {
+      request(app)
+        .post('/math/divide')
+        .send({ num1: 'not number', num2: 'not number' })
+        .expect(httpStatus.BAD_REQUEST)
+        .end((error) => (error) ? done.fail(error) : done())
+    })
+
+    it('should return 400 if num2 equals to 0', (done) => {
+      request(app)
+        .post('/math/divide')
+        .send({ num1: 11, num2: 0 })
+        .expect(httpStatus.BAD_REQUEST)
+        .end((error) => (error) ? done.fail(error) : done())
+    })
+
+    it('should return 200 and result successfully if num1 or num2 are number', (done) => {
+      request(app)
+        .post('/math/divide')
+        .send({ num1: 6, num2: 2 })
         .expect({ result: 3 })
         .expect(httpStatus.OK)
         .end((error) => (error) ? done.fail(error) : done())
